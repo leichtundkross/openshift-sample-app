873cec5fc05ce7545bd592b095a178215724c0a3
Andreas Günzel <andreas.guenzel@exxeta.com>
2018-04-20 13:00:46 +0200

    removed ip stuff

--- src/main/java/de/exxeta/angu/Application.java
+++ src/main/java/de/exxeta/angu/Application.java
@@ -1,8 +1,5 @@
 package de.exxeta.angu;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +13,6 @@ public class Application {
    @RequestMapping("/")
    public String home() {
       return "Hello Openshift World";
-   }
-
-   @RequestMapping("/ip")
-   public String getIp() throws UnknownHostException, InterruptedException {
-      Thread.sleep(1000);
-      InetAddress inetAddress = InetAddress.getLocalHost();
-      return inetAddress.getHostAddress();
    }
 
    public static void main( String[] args ) {
